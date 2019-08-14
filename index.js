bb231cdb8a211da8e47f3ee8730770c25131f1c8
Michael Chen <mjchen@Michaels-MacBook-Pro.local>
2019-08-14 10:21:10 -0700

    Unified js script for localhost testing

--- index.js
+++ index.js
@@ -1,13 +1,74 @@
-var http = require('http');
+// Import express and request modules
+var express = require('express');
+var request = require('request');
 
-const PORT = 3000;
+// Store our app's ID and Secret. These we got from Step 1. 
+// For this tutorial, we'll keep your API credentials right here. But for an actual app, you'll want to  store them securely in environment variables. 
+var clientId = process.env.SLACK_CLIENT_ID;
+var clientSecret = process.env.SLACK_CLIENT_SECRET;
 
-function handleRequest(request, response) {
-	response.end('Ngrok is working! - Path Hit: ' + request.url);
-}
+// Instantiates Express and assigns our app variable to it
+var app = express();
 
-var server = http.createServer(handleRequest);
+// stuff from slack support
 
-server.listen(PORT, function() {
-	console.log("Server listening on: http://localhost:%s", PORT);
+const bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support parsing of application/json type post data
+
+// end stuff from slack support
+
+
+// Again, we define a port we want to listen to
+const PORT=3000;
+
+// Lets start our server
+app.listen(PORT, function () {
+	//Callback triggered when server is successfully listening. Hurray!
+	console.log("Example app listening on port " + PORT);
+});
+
+
+// This route handles GET requests to our root ngrok address and responds with the same "Ngrok is working message" we used before
+app.get('/', function(req, res) {
+	res.send('Ngrok is working! Path Hit: ' + req.url);
+});
+
+// This route handles get request to a /oauth endpoint. We'll use this endpoint for handling the logic of the Slack oAuth process behind our app.
+app.get('/oauth', function(req, res) {
+	// When a user authorizes an app, a code query parameter is passed on the oAuth endpoint. If that code is not there, we respond with an error message
+	if (!req.query.code) {
+		res.status(500);
+		res.send({"Error": "Looks like we're not getting code."});
+		console.log("Looks like we're not getting code.");
+	} else {
+		// If it's there...
+
+		// We'll do a GET call to Slack's `oauth.access` endpoint, passing our app's client ID, client secret, and the code we just got as query parameters.
+		request({
+			url: 'https://slack.com/api/oauth.access', //URL to hit
+			qs: {code: req.query.code, client_id: clientId, client_secret: clientSecret}, //Query string data
+			method: 'GET', //Specify the method
+
+		}, function (error, response, body) {
+			if (error) {
+				console.log(error);
+			} else {
+				res.json(body);
+
+			}
+		})
+	}
+});
+
+// Route the endpoint that our slash command will point to and send back a simple response to indicate that ngrok is working
+app.post('/command', function(req, res) {
+	res.send('Your ngrok tunnel is up and running!');
+});
+
+// handle slack challenge parameter
+app.post('/slack/events', function(req, res) {
+	//res.status(200);
+	//res.set('Content-Type', 'application/json');
+	console.log(req.body);
+	res.send(req.body.challenge);
 });
